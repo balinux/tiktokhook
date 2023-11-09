d9451baddefa7afc5f8a96796c64cc1b94cb0cdd
balinux <ryojuniyantara@gmail.com>
2023-11-09 15:20:20 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000; // You can use any port you like
+
+// Use body-parser middleware to parse JSON data from incoming requests
+// app.use(bodyParser.json());
+
+// Use body-parser middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define a route for your webhook endpoint (e.g., /webhook)
+app.post('/webhook', (req, res) => {
+  // Process the data from the webhook payload here
+  console.log('Webhook received:', req.body);
+
+  // Respond to the webhook request
+  res.status(200).send('Webhook received successfully');
+});
+
+app.get('/',(req, res) => {
+  res.send('halo wardana')
+})
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Webhook server is running on port ${port}`);
+});
